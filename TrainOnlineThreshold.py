32b0d3417116c0c081646d35bdbfb3b733ce9f70
Yael Galifat <yael_432@hotmail.com>
2019-06-01 15:47:57 +0300

    Update TrainOnlineThreshold.py

--- TrainOnlineThreshold.py
+++ TrainOnlineThreshold.py
@@ -8,6 +8,7 @@ import numpy as np
 from Utils.ExportData import ExportOnlineTraining
 import TrainBranchyModelExpCertWeights
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 #global process evaluating parameters
 onlieExmaples = 50000
@@ -16,7 +17,7 @@ trainLastExamplesToCheck = 200
 convergenceThreshold = 0.005
 onlineTrainingSummary = ExportOnlineTraining(printToLog=False)
 certaintyLevels =  [0,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75, 0.8,0.85,0.9,0.95,0.99,0.999]
-shuffleLevels = [50000,10000,5000,3000,1000,750,500,50,10,5,1]
+shuffleLevels = [1,50000] #[50000,10000,5000,3000,1000,750,500,50,10,5,1]
 
 def prepareData(shuffleLevel = 50000):
     CIFAR10 = CIFAR10data()
@@ -32,28 +33,31 @@ def prepareData(shuffleLevel = 50000):
         newExample = OnlineExample(data,target)
         onlineData.append(newExample)
 
-    onlineDataSorted = sorted(onlineData, key=lambda x: x.target)
+    if shuffleLevel > 1:
+        onlineDataSorted = sorted(onlineData, key=lambda x: x.target)
 
-    if shuffleLevel < 50000:
-        # shuffle
-        onlineDataSorted_copy = onlineDataSorted[::shuffleLevel]
-        random.shuffle(onlineDataSorted_copy)
-        onlineDataSorted[::shuffleLevel] = onlineDataSorted_copy
-
+        if shuffleLevel < 50000:
+            # shuffle
+            onlineDataSorted_copy = onlineDataSorted[::shuffleLevel]
+            random.shuffle(onlineDataSorted_copy)
+            onlineDataSorted[::shuffleLevel] = onlineDataSorted_copy
+    else:
+        onlineDataSorted = onlineData[:]
 
 
     return onlineDataSorted
 
 def prepareNetwork():
     #load alex branchy net
-    brancyNet,optimizer,scheduler,CIFAR10 = TrainBranchyModelExpCertWeights.defineNetwork(True,lr=0.001,weight_decay=0.3)
+    brancyNet,optimizer,scheduler,CIFAR10 = TrainBranchyModelExpCertWeights.defineNetwork(True,lr=0.01)
     return brancyNet,optimizer
 
 def lossFunction(branchyNetwork,branchyNetworkOutput,target):
     loss, lossByPath, correctByPath, certScorePyPath = \
         TrainBranchyModelExpCertWeights.LossFunctionCrossEntropyAndBCE(branchyNetwork,branchyNetworkOutput,target)
     #3 - path index in current branch cofiguration
-    return loss, lossByPath[3], correctByPath[3], certScorePyPath[3]
+    #5 - 1 original example + 4 synthetic data
+    return loss, lossByPath[3], correctByPath[3]/5, certScorePyPath[3]/5
 
 
 def trainOnline(branchyNetwork,optimizer,onlineData,shuffleLevel = 50000):
@@ -86,27 +90,44 @@ def trainOnline(branchyNetwork,optimizer,onlineData,shuffleLevel = 50000):
         idx += 1
         batchidx = math.floor(idx/trainLastExamplesToCheck)
 
-        optimizer.zero_grad()
+        #optimizer.zero_grad()
 
-        data= example.dataTensor.to(device)
+        dataAug = torch.zeros([5, example.dataTensor.size()[1], example.dataTensor.size()[2],example.dataTensor.size()[3]], dtype=torch.float)
+
+        dataAug[0,:,:,:] = example.dataTensor
+        dataAug[1, :, :, :] = torch.rot90(example.dataTensor, 1, [2, 3])
+        dataAug[2, :, :, :] = torch.rot90(example.dataTensor, 2, [2, 3])
+        dataAug[3, :, :, :] = torch.rot90(example.dataTensor, 3, [2, 3])
+        dataAug[4, :, :, :] = torch.flip(example.dataTensor, [2, 3])
+        # plt.imshow(dataAug[4, :, :, :].permute(1, 2, 0))
+        # plt.show()
+
+        data= dataAug.to(device)
 
         output = branchyNetwork(data)
 
 
 
-        target = output['main'].networkOutput.data.max(1, keepdim=True)[1]
-        target = torch.squeeze(target).unsqueeze(dim=0)
+        targetClass = output['main'].networkOutput.data.max(1, keepdim=True)[1]
+        targetClass = torch.squeeze(targetClass).unsqueeze(dim=0)
+
+        trueClass = targetClass.data[0,0].item()
+
+        target = torch.empty(5, 1, dtype=torch.long)
+        target = target.fill_(trueClass)
+        target = torch.squeeze(target)
+        target = target.to(device)
 
         loss, lossByPath, correct, certScor = lossFunction(branchyNetwork,
                                                             output,
                                                             target)
 
-        trueClass = target.item()
+        loss = loss*(1/50000)
         exploreClassesPerBatch[0, trueClass] += 1
 
-        if inferencePhase:
-            lossCoefficient = 1- exploreClassesPerBatch[0, trueClass]/np.sum(exploreClassesPerBatch)
-            loss = loss * lossCoefficient
+        # if inferencePhase:
+        #     lossCoefficient = 1- exploreClassesPerBatch[0, trueClass]/np.sum(exploreClassesPerBatch)
+        #     loss = loss * lossCoefficient
 
         exampleScore = (correct + certScor)/2
         batchScore += exampleScore
@@ -115,7 +136,7 @@ def trainOnline(branchyNetwork,optimizer,onlineData,shuffleLevel = 50000):
 
         if inferencePhase:
             for cert in certaintyLevels:
-                if output[3].networkCertainty.item() >= cert:
+                if output[3].networkCertainty.data[0,0].item() >= cert:
                     inferenceAccuracyByCert[cert] += correct
                     casesToEvalByCert[cert] += 1
 
@@ -149,6 +170,8 @@ def trainOnline(branchyNetwork,optimizer,onlineData,shuffleLevel = 50000):
                     inferencePhase = True
                     inferenceTime = idx
                     onlineTrainingSummary.saveInferenceTime(shuffle=shuffleLevel,inferenceTime=inferenceTime)
+
+            optimizer.zero_grad()
 
         loss.backward()
         optimizer.step()
