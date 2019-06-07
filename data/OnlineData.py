88059044ba2ed6a4b16921e6245a7fa26f2b66a4
Yael Galifat <yael_432@hotmail.com>
2019-06-07 18:43:23 +0300

    Update OnlineData.py

--- data/OnlineData.py
+++ data/OnlineData.py
@@ -1,5 +1,6 @@
 class OnlineExample():
-    def __init__(self, example_data,example_target):
+    def __init__(self, example_data,example_target,batch_idx):
         self.dataTensor = example_data.clone()
         self.LabelTensor = example_target.clone()
         self.target = example_target.item()
+        self.exampleKey = batch_idx
