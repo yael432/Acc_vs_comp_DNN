b29923ed09f94fbd779c25f41ea95e6e482580f5
Yael Galifat <yael_432@hotmail.com>
2019-10-15 19:33:31 +0300

    Update EvalBranchyModel.py

--- EvalBranchyModel.py
+++ EvalBranchyModel.py
@@ -4,6 +4,8 @@ from Utils.ExportData import ExportBranchyNetEval
 from Utils import LoadNets
 from Models.LeNet.TrainMNIST import MNISTdata
 from Models.AlexNet.TrainAlexNet import CIFAR10data
+import TrainBranchyModelExpCertWeights
+import argparse
 
 def eval(BrancyNetwork, test_loader, exportEvalData, certaintyThreshold=0.5,GroundTruthAvailable = True):
     BrancyNetwork.eval()
@@ -186,25 +188,58 @@ def evalBranchyAlexNet(resultsPath,lossFunctionID,GroundTruthAvailable = True):
 
     exportEvalData.saveData(resultsPath)
 
+def evalDefalutBranchyAlexNet():
+    branchyNet, optimizer, scheduler, CIFAR10 = TrainBranchyModelExpCertWeights.defineNetwork(True, lr=0.01)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    branchyNet.to(device)
+
+    # set test dataset
+    CIFAR10 = CIFAR10data()
+
+    CIFAR10.prepareCIFAR10TestDataset(batch_size_test=100)
+
+    exportEvalData = ExportBranchyNetEval()
+
+    certaintyLevels = [0, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9,
+                       0.95, 0.99, 0.999]
+    for certainty in certaintyLevels:
+        print("eval default alexnet certainty {}".format(certainty))
+        eval(branchyNet, CIFAR10.test_loader, exportEvalData, certainty, GroundTruthAvailable=True)
+
+    exportEvalData.saveData("Results/eval default network")
 if __name__ == '__main__':
-    #evalBranchyLeNet('Results/LeNetBranch first/EvalResults')
-    #evalBranchyLeNet('Results/LeNetBranch first no ground truth/EvalResults',GroundTruthAvailable=False)
-    #evalBranchyAlexNet('Results/AlexNetBranch/EvalResults')
-    # evalBranchyAlexNet('Results/Loss function 1 main to certainty/AlexNetBranch no ground truth',
-    #                    GroundTruthAvailable=False,
-    #                    lossFunctionID=1) #'Results/AlexNetBranch no ground truth/EvalResults'
-    # evalBranchyAlexNet('Results/Loss function 2 main to certainty/AlexNetBranch no ground truth',
-    #                    GroundTruthAvailable=False,
-    #                    lossFunctionID=2)
-    # evalBranchyAlexNet('Results/Loss function 3 main to certainty/AlexNetBranch no ground truth',
-    #                    GroundTruthAvailable=False,
-    #                    lossFunctionID=3)  # 'Results/AlexNetBranch no ground truth/EvalResults'
-    evalBranchyLeNet('Results/Loss function 1 main to certainty/LeNetBranch no ground truth',
-                       GroundTruthAvailable=False,
-                       lossFunctionID=1)
-    evalBranchyLeNet('Results/Loss function 2 main to certainty/LeNetBranch no ground truth',
-                     GroundTruthAvailable=False,
-                     lossFunctionID=2)
-    evalBranchyLeNet('Results/Loss function 3 main to certainty/LeNetBranch no ground truth',
-                     GroundTruthAvailable=False,
-                     lossFunctionID=3)
+
+    parser = argparse.ArgumentParser(description='Learning with LM-LSTM-CRF together with Language Model')
+    parser.add_argument('--lossFunctionID', type=int, default=2, help='choose loss function for training')
+    parser.add_argument('--netArct', default="AlexNet", help='which model to train')
+    args = parser.parse_args()
+
+    if args.netArct == "AlexNet":
+        if args.lossFunctionID == 1:
+            evalBranchyAlexNet('Results/Loss function 1 main to certainty/AlexNetBranch no ground truth',
+                               GroundTruthAvailable=False,
+                               lossFunctionID=1) #'Results/AlexNetBranch no ground truth/EvalResults'
+        if args.lossFunctionID == 2:
+            evalBranchyAlexNet('Results/Loss function 2 main to certainty/AlexNetBranch no ground truth',
+                               GroundTruthAvailable=False,
+                               lossFunctionID=2)
+        if args.lossFunctionID == 3:
+            evalBranchyAlexNet('Results/Loss function 3 main to certainty/AlexNetBranch no ground truth',
+                               GroundTruthAvailable=False,
+                               lossFunctionID=3)  # 'Results/AlexNetBranch no ground truth/EvalResults'
+        if args.lossFunctionID == 0:
+            evalDefalutBranchyAlexNet()
+    else:
+        if args.lossFunctionID == 1:
+            evalBranchyLeNet('Results/Loss function 1 main to certainty/LeNetBranch no ground truth',
+                           GroundTruthAvailable=False,
+                           lossFunctionID=1)
+        if args.lossFunctionID == 2:
+            evalBranchyLeNet('Results/Loss function 2 main to certainty/LeNetBranch no ground truth',
+                         GroundTruthAvailable=False,
+                         lossFunctionID=2)
+        if args.lossFunctionID == 3:
+            evalBranchyLeNet('Results/Loss function 3 main to certainty/LeNetBranch no ground truth',
+                         GroundTruthAvailable=False,
+                         lossFunctionID=3)
+
